809cab056e2a4b5f030d532f3f083f4108ece0fe
Oli Newsham <oli.newsham@gmail.com>
2018-10-27 19:28:49 +0100

    content encoder

---
+++ web-api/WebApi/WebApi/Models/ContentFlags.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+
+namespace WebApi.Models
+{
+    public class ContentFlags
+    {
+        private readonly BitArray _flags;
+
+        public ContentFlags()
+        {
+            _flags = new BitArray(8); 
+        }
+
+        public void SetPlaintext()
+        {
+            _flags[0] = false;
+        }
+
+        public void SetMarkup()
+        {
+            _flags[0] = true;
+        }
+
+        public string GetValue()
+        {
+            var  array = new int[1];
+            _flags.CopyTo(array, 0);
+            return array[0].ToString("X2");
+            
+        }
+    }
+}
--- web-api/WebApi/WebApi/Utilities/ContentEncoder.cs
+++ web-api/WebApi/WebApi/Utilities/ContentEncoder.cs
@@ -2,43 +2,41 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using WebApi.Models;
 
 namespace WebApi.Utilities
 {
-    //public static class ContentEncoder
-    //{
-    //    private const int ChunkSize = 148; //160 - 12;
+    public static class ContentEncoder
+    {
+        private const int ChunkSize = 148; //160 - 12;
 
+        public static IReadOnlyList<string> GeneratePlainText(string content, string sessionId)
+        {
+            var flags = new ContentFlags();
 
-    //    public static IReadOnlyList<string> GeneratePlainText(string content, string sessionId)
-    //    {
-    //        var contentChunks = Enumerable.Range(0, content.Length / ChunkSize).Select(i => content.Substring(i * ChunkSize, ChunkSize));
-    //        var content = new List<string>();
+            flags.SetPlaintext();
 
-    //        var totalFrames = System.Text.Encoding.UTF8.GetString(Convert.ToByte(content.Length));
+            return GeneratePlaintextInternal(content, sessionId, flags);
+        }
 
-    //        for (var i = 0; i < contentChunks.Length; i++) {
+        public static IReadOnlyList<string> GeneratextMarkup(string content, string sessionId)
+        {
+            var flags = new ContentFlags();
+
+            flags.SetMarkup();
+
+            return GeneratePlaintextInternal(content, sessionId, flags);
+        }
+
+        private static IReadOnlyList<string> GeneratePlaintextInternal(string content, string sessionId, ContentFlags flags)
+        {
+            var contentChunks = Enumerable.Range(0, content.Length / ChunkSize).Select(i => content.Substring(i * ChunkSize, ChunkSize)).ToArray();
+
+            var totalFrameString = contentChunks.Length.ToString("X2");
+
             
-    //            var currentFrame = System.Text.Encoding.UTF8.GetString(Convert.ToByte(i));
-                
-    //            content.Add($"OK {sessionId}{currentFrame}{totalFrames} {contentChunks}")
 
-
-
-
-
-    //        }
-
-    //        return new List<string>();
-
-
-
-
-
-
-
-
-
-    //    }
-    //}
+            return contentChunks.Select((t, i) => $"OK {sessionId}{i:X2}{totalFrameString}{flags.GetValue()} {t}").ToList();
+        }
+    }
 }
